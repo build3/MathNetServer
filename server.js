9a35686e9f2a95f9d6982327c9a087db244cff4c
Paul Salessi <pasalessi@ucdavis.edu>
2015-07-28 14:35:33 -0700

    Express-session module was causing server to crash. Commented out
    Moved location of admin.html. Currently, routing is not working properly

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ var port     = 8888;
 var path = require('path');
 
 //var passport = require('passport');
-var session = require('express-session');
+//var session = require('express-session');
 
 //require('./passport')(passport); 
 
-var bodyParser = require('body-parser'); // for reading POSTed form data into `req.body`
-var cookieParser = require('cookie-parser'); 
+//var bodyParser = require('body-parser'); // for reading POSTed form data into `req.body`
+//var cookieParser = require('cookie-parser'); 
 
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+//app.use(cookieParser()); // read cookies (needed for auth)
+//app.use(bodyParser()); // get information from html forms
 
-app.use(session({ secret: 'vidyapathaisalwaysrunning' } )); // session secret
+//app.use(session({ secret: 'vidyapathaisalwaysrunning' } )); // session secret
 //app.use(passport.initialize());
 //app.use(passport.session()); // persistent login sessions
 
