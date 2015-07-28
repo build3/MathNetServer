5b714c51f39ebab1ca34f194693618378fd05472
Anton Ochs <asochs@ucdavis.edu>
2015-07-28 10:24:14 -0700

    Added basic html to the student view files, added routes to load these html files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/index');
+    res.redirect('/index');
+});
+router.post('/', function(request, response){
+    var student_class = request.body.class_id;
+    var student_nick = request.body.nickname;
+    //add these variables into the datastructure
+    //redirect to the appropriate class
+});
+router.get('/class', function(request, response){
+    response.redirect('/class');
+});
+router.get('/groups', function(request, response){
+    response.redirect('/groups');
 });
 
 module.exports = router;
