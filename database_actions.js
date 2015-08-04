68290f9de74a760939634e5f30ffd459599d2d81
Paul Salessi <pasalessi@ucdavis.edu>
2015-08-04 15:58:21 -0700

    Create class database function now has group creation as a callback.
    This prevents race conditions as the callback is not executed until the
    insert query completes.

--- database_actions.js
+++ database_actions.js
@@ -11,15 +11,38 @@ var pool = mysql.createPool({
 });
 
 // Creates row in Classes table using class_name
-exports.create_class = function(class_name) {
+exports.create_class = function(class_name, group_count) {
     pool.getConnection(function(error, connection) {
         console.log("Creating " + class_name);
         var query = "USE nsf_physics_7;";
         connection.query(query);
 
         query = "INSERT INTO Classes (class_name) VALUES (?);";
-        connection.query(query, [class_name]);
-
+        connection.query(query, [class_name], function(error, rows) {
+            if (error) {
+                console.log("Failed to make class");
+            }
+            else {
+                // If the creation of the class was successful,
+                //   the callback then gets the class_id
+                //   and makes group_count many groups
+                var class_id;
+                query = "SELECT class_id FROM Classes WHERE class_name=?;";
+                connection.query(query, [class_name], function(error, rows) {
+                    if(error) {
+                        console.log("Failed to make groups");
+                    }
+                    else {
+                        class_id = rows[0].class_id;
+                        for (var group=1; group < group_count + 1; group++) {
+                            query = 
+                                "INSERT INTO Groups (group_id, class_id) VALUES (?, ?);"
+                            connection.query(query, [group, class_id]);
+                        }
+                    }
+                });
+            }
+        });
         connection.release();
     });
 }
