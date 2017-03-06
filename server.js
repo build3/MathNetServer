ae17b18f2e208422c8ea50f0194079d57a3d4fa1
Simon Dvorak <sidvorak@ucdavis.edu>
2017-03-06 09:04:03 -0800

    DELETED: removed keep alive attempt and set server timeout to 0

--- server.js
+++ server.js
@@ -3,17 +3,9 @@
 var port     = 8889;
 var server_sockets = require('./server_sockets');
 var http = require('http');
-var server = http.createServer(
-function (req, res) {
-  setTimeout(function() {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-  }, 200);
-}
-	).listen(port);
-server.timeout = 0;
-server.setTimeout(600000);
-console.log(server.timeout);
+var server = http.createServer().listen(port);
+
+server.setTimeout(0);
 console.log('The magic happens on port ' + port);
 
 server_sockets(server, "");
