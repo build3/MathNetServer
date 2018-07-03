eb510f2cf806942c2df569daa72e22aa343634eb
Jeroen Post <jjpost@ucdavis.edu>
2018-07-03 09:47:29 -0700

    Creating arguments for port

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 "use strict";
 //server.js
-var port     = 8889;
+
+// Grab arguments given
+var args = process.argv.slice(2);
+
+var port = 8889;
+
+// If we have an argument, use that as the port instead
+if(args[2]){
+    port = args[2];
+}
+
 var server_sockets = require('./server_sockets');
 var http = require('http');
 var server = http.createServer().listen(port);
