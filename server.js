e5fececa71a0ca2aa5956fc31460d37717432132
Jeroen Post <jjpost@ucdavis.edu>
2018-07-05 16:29:46 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ var args = process.argv.slice(2);
 var port = 8889;
 
 // If we have an argument, use that as the port instead
-if(args[2]){
-    port = args[2];
+// 8889 for live
+// 8888 for test
+// 8887 for dev
+
+if(args[0]){
+    port = args[0];
 }
 
 var server_sockets = require('./server_sockets');
